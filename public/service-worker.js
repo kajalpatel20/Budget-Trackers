c1ad3302328e659a60280408dfb1f87f72ba46bf
Kajal Patel <kajalpatel20@gmail.com>
2021-04-27 20:33:10 -0400

    deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,19 +3,13 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    './index.html',
-    './css/styles.css',
-    './js/index.js',
-    './js/idb.js',
-    './manifest.json',
-    './icons/icon-512x512.png',
-    './icons/icon-384x384.png',
-    './icons/icon-192x192.png',
-    './icons/icon-152x152.png',
-    './icons/icon-144x144.png',
-    './icons/icon-128x128.png',
-    './icons/icon-96x96.png',
-    './icons/icon-72x72.png'
+    '/index.html',
+    '/styles.css',
+    '/index.js',
+    '/idb.js',
+    '/manifest.json',
+    '/icons/icon-512x512.png',
+    '/icons/icon-192x192.png'
 ];
 
 self.addEventListener('install', function (e) {
@@ -37,7 +31,7 @@ self.addEventListener('activate', function (e) {
 
             return Promise.all(keyList.map(function (key, i) {
                 if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i] );
+                    console.log('deleting cache : ' + keyList[i]);
                     return caches.delete(keyList[i]);
                 }
             }));
